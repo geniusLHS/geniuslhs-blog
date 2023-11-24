545dcb07c646f94c75f22fbbb445ccab3b6956e5
Hyunseo Lee <cube040318@gmail.com>
2023-11-25 00:18:35 +0900

    Revert "feat: next-seo 사용해 SEO.tsx 작성 및 실제 Blog page에 적용"
    This reverts commit bbc80e7d7f0b489b088a85b682719925defd62b6.

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,8 +1,7 @@
-import { allPosts } from "contentlayer/generated";
-import { notFound } from "next/navigation";
-import { getMDXLayout, postComponents } from "@/components/mdx-components";
-import dayjs from "dayjs";
-import { PageSEO, BlogSEO } from "@/components/SEO";
+import { allPosts } from 'contentlayer/generated';
+import { notFound } from 'next/navigation';
+import { getMDXLayout, postComponents } from '@/components/mdx-components';
+import dayjs from 'dayjs';
 
 interface Props {
   params: {
@@ -12,12 +11,12 @@ interface Props {
 
 export const generateStaticParams = async () => {
   return allPosts.map((post) => ({
-    slug: post._raw.sourceFileName.split(".mdx")[0],
+    slug: post._raw.sourceFileName.split('.mdx')[0],
   }));
 };
 
 export default function PostLayout({ params }: Props) {
-  const currentPost = allPosts.find((post) => post._raw.sourceFileName.split(".mdx")[0] === params.slug && post.category == "Blog");
+  const currentPost = allPosts.find((post) => post._raw.sourceFileName.split('.mdx')[0] === params.slug && post.category == 'Blog');
 
   if (!currentPost) {
     notFound();
@@ -27,11 +26,10 @@ export default function PostLayout({ params }: Props) {
 
   return (
     <>
-      <BlogSEO title={currentPost.title} summary={currentPost.description} date={currentPost.date} url={currentPost.url} />
       <div className="text-center pt-8 pb-8 mb-4 border-b border-b-[#898ea4]">
         <h1 className="text-4xl font-bold pb-5">{currentPost.title}</h1>
         <p className="text-lg">{currentPost.description}</p>
-        <p className="text-gray-500">{dayjs(currentPost.date).format("YYYY. MM. DD")}</p>
+        <p className="text-gray-500">{dayjs(currentPost.date).format('YYYY. MM. DD')}</p>
       </div>
       <article className="w-full prose">
         <MDXLayout components={postComponents} />
