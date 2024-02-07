b902b0f2c5fa3e2a39e976333d9139481ea7596d
Hyunseo Lee <cube040318@gmail.com>
2024-02-07 18:41:33 +0900

    design: 연도 24년으로 수정

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,12 +1,12 @@
-'use client';
+"use client";
 
-import { GithubIcon } from '@/styles/SvgIcons';
+import { GithubIcon } from "@/styles/SvgIcons";
 
 export default function Footer() {
   return (
     <footer className="mt-10 w-full border-t border-t-[#898ea4] pt-10 text-center text-sm text-gray-500">
       <div className="flex justify-center items-center">
-        © 2023 geniusLHS
+        © 2024 geniusLHS
         <a
           className="flex justify-center items-center rounded-full mx-2 p-2 transition-all cursor-pointer hover:bg-gray-100"
           href="https://github.com/geniuslhs"
