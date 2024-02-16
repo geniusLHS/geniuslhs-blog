33f36a95888f84420b387e8ee63911404308e1a3
Hyunseo Lee <cube040318@gmail.com>
2024-02-17 01:13:05 +0900

    feat: supabase 도입, 미들웨어 및 view api 코드 작성

---
+++ src/middleware.ts
@@ -0,0 +1,33 @@
+import { NextMiddleware, NextResponse } from "next/server";
+import type { NextRequest, NextFetchEvent } from "next/server";
+
+export const middleware: NextMiddleware = async (request: NextRequest, event: NextFetchEvent) => {
+  const pathname = request.nextUrl.pathname;
+
+  const sendAnalytics = async () => {
+    const slug = pathname.slice(pathname.indexOf("/")) || "/";
+
+    const URL = process.env.NODE_ENV === "production" ? "https://geniuslhs.com/api/view" : "http://localhost:3000/api/view";
+    console.log(URL);
+    const res = await fetch(`${URL}`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        slug,
+      }),
+    });
+
+    if (res.status !== 200) {
+      console.error("Failed to send analytics", res);
+    }
+  };
+
+  event.waitUntil(sendAnalytics());
+  return NextResponse.next();
+};
+
+export const config = {
+  matcher: ["/blog/:path+", "/activity/:path+"],
+};
