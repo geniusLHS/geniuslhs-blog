87626ff30be4d330e84ba13898dbf09522aa50a2
Hyunseo Lee <cube040318@gmail.com>
2024-02-17 18:08:39 +0900

    refactor: middleware 삭제, 해당 로직 page.tsx로 이동

--- src/app/api/view/route.ts
+++ src/app/api/view/route.ts
@@ -2,7 +2,8 @@ import supabase from "@/lib/supabase/private";
 
 export async function POST(request: Request) {
   try {
-    const { slug, ip } = await request.json();
+    const { slug } = await request.json();
+    const ip = (request.headers.get("x-forwarded-for") ?? "127.0.0.1").split(",")[0];
 
     await supabase.rpc("new_visitor", { page_slug: slug, user_ip: ip });
   } catch (error) {
