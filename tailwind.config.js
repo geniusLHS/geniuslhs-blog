6067877db0f6634801a662cca6f26ae1a64f9c46
Hyunseo Lee <cube040318@gmail.com>
2024-08-27 19:06:39 +0900

    style: 모바일용 프리텐다드 폰트 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        pretendard: ["Pretendard"],
+        pretendard: ["Pretendard", "Pretendard-Mobile"],
       },
     },
   },
