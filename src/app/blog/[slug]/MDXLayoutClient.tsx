f194d750f96e7bf5966f2c271b5533533a7631d9
Hyunseo Lee <cube040318@gmail.com>
2025-01-05 03:15:45 +0900

    feat: MDXLayoutClient 컴포넌트에서 테스트 해보면 컴포넌트 삭제

--- src/app/blog/[slug]/MDXLayoutClient.tsx
+++ src/app/blog/[slug]/MDXLayoutClient.tsx
@@ -1,16 +1,8 @@
 "use client";
 
 import { getMDXLayout, postComponents } from "@/components/mdx-components";
-import UseContext from "@/components/posts/minimal-rerendering-react-form/containers/WithContextAPI";
 
 export default function MDXLayoutClient({ code }: { code: string }) {
   const MDXLayout = getMDXLayout(code);
-  return (
-    <>
-      <div className="">
-        <UseContext />
-        <MDXLayout components={postComponents} />
-      </div>
-    </>
-  );
+  return <MDXLayout components={postComponents} />;
 }
